cfd37209738d16731f573d91c93253f7eb9b498a
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-05-05 18:31:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,6 +17,6 @@ def speak_to_grandma(phrase)
   puts "HUH?! SPEAK UP, SONNY!"
 elsif phrase == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN"
-else phrase != String.downcase
-  puts "NO, NOT SINCE 1938"
+else phrase != phrase.downcase
+  puts "NO, NOT SINSCE 1938"
   end
